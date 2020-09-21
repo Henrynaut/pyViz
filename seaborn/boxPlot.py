6a404db5aaf20f2e10c090d0d1f2aadaf31af14f
Neil McHenry <neilmchenry@gmail.com>
2020-09-20 23:34:25 -0500

    Update boxPlot.py
    Added swarmplot comment

--- seaborn/boxPlot.py
+++ seaborn/boxPlot.py
@@ -5,4 +5,5 @@ tips = sns.load_dataset("tips")
 
 ax = sns.boxplot(x="day", y="total_bill", hue="smoker",
                     data=tips, palette="Set3")
+# ax = sns.swarmplot(x="day", y="total_bill", data=tips, color=".25")
 plt.show()
