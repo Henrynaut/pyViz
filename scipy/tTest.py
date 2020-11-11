2d56bbfbeb4584e5c86e8e0225f13ed6bbf1c733
Neil McHenry <neil.g.mchenry@nasa.gov>
2020-11-11 12:09:52 -0600

    Added confidence interval lines

--- scipy/tTest.py
+++ scipy/tTest.py
@@ -1,3 +1,5 @@
+#Derived from https://datasciencechalktalk.com/2019/09/02/hypothesis-tests-with-python/
+
 import numpy as np
 
 # Create population with mean of 1.5
@@ -18,6 +20,35 @@ plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) *np.exp( - (bins - mu)**2 / (2 * s
 sample_count, sample_bins, sample_ignored = plt.hist(sample, 30, alpha=0.1, color='r',density=True)
 plt.plot(sample_bins,1/(sample_sigma * np.sqrt(2 * np.pi)) *np.exp( - (sample_bins - sample_mean)**2 / (2 * sample_sigma**2) ),linewidth=2, color='r')
 plt.plot(bins,1/(sigma * np.sqrt(2 * np.pi)) *np.exp( - (bins - mu)**2 / (2 * sigma**2) ),linewidth=2, color='b')
+
+#Plot 95% confidence interval lines
+import scipy
+from scipy import stats, optimize, interpolate
+ci = scipy.stats.norm.interval(0.95, loc=1.5, scale=2)
+plt.axvline(ci[0],color='g')
+plt.axvline(ci[1],color='g')
+
+#Label possible outcomes of test
+plt.fill_between(x=np.arange(-4,ci[0],0.01), 
+                 y1= scipy.stats.norm.pdf(np.arange(-4,ci[0],0.01),loc=1.5,scale=2) ,
+                 facecolor='red',
+                 alpha=0.35)
+
+plt.fill_between(x=np.arange(ci[1],7.5,0.01), 
+                 y1= scipy.stats.norm.pdf(np.arange(ci[1],7.5,0.01),loc=1.5,scale=2) ,
+                 facecolor='red',
+                 alpha=0.5)
+
+plt.fill_between(x=np.arange(ci[0],ci[1],0.01), 
+                 y1= scipy.stats.norm.pdf(np.arange(ci[0],ci[1],0.01),loc=3, scale=2) ,
+                 facecolor='blue',
+                 alpha=0.5)
+
+plt.text(x=0, y=0.18, s= "Null Hypothesis")
+plt.text(x=6, y=0.05, s= "Alternative")
+plt.text(x=-4, y=0.01, s= "Type 1 Error")
+plt.text(x=6.2, y=0.01, s= "Type 1 Error")
+plt.text(x=2, y=0.02, s= "Type 2 Error")
 
 # Render all plots
 plt.show()
